42e9830c4f3098275f1a1940446dedcc4fa2a6d9
Robin de Bruin <robin_de_bruin@live.nl>
2022-11-18 13:43:46 +0100

    r space switch

--- keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
+++ keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  | MOUSE|  ] } |   N  |   M  | ,  < | . >  | /  ? | esc/RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Option| CMD | LCtrl/| Space| Nmbrs|  | Nav  | Space|Option| CMD  |  ~   |
+ *                        |Option| CMD | LCtrl/| Space| Nmbrs|  | Nav  |Option| Space| CMD  |  ~   |
  *                        |   ⌥  |      | Enter|      | layer|  | Layer|      |   ⌥  |      |  `   |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                                     KC_Y,    KC_U ,   KC_I ,   KC_O ,  KC_P ,   KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                                     KC_H,    KC_J ,   KC_K ,   KC_L ,  KC_SCLN, CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,          KC_LBRC, KC_CAPS,         TG(4) , KC_RBRC, KC_N,    KC_M ,   KC_COMM, KC_DOT ,KC_SLSH, RSA_T(KC_ESC),
-                               KC_LALT , KC_LGUI, LCTL_T(KC_ENTER), KC_SPC , MO(1),           MO(2) , KC_SPC , KC_RALT, KC_RGUI, KC_GRV
+                               KC_LALT , KC_LGUI, LCTL_T(KC_ENTER), KC_SPC , MO(1),           MO(2) , KC_RALT, KC_SPC ,  KC_RGUI, KC_GRV
     ),
 
 /*
