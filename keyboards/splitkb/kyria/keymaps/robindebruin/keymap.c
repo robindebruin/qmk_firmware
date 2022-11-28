46e353e018cfc126147303bff38236ab63d0a065
Robin de Bruin <robin_de_bruin@live.nl>
2022-11-28 22:13:43 +0100

    syms

--- keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
+++ keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
@@ -63,35 +63,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |MOUSE |  |CapsLk|  ] } |   N  |   M  | ,  < | . >  | /  ? | esc/RAltShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Option| CMD | LCtrl/| Space| Nmbrs|  | Nav  |Option| Space| CMD  |  ~   |
- *                        |   ⌥  |      | Enter|      | layer|  | Layer|      |   ⌥  |      |  `   |
+ *                        |LCtrl/| alt  | cmd  | enter| Nmbrs|  | Nav  |Option| Space| CMD  |  ~   |
+ *                        |   ⌥  |      |      |      | layer|  | Layer|      |   ⌥  |      |  `   |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                                     KC_Y,    KC_U ,   KC_I ,   KC_O ,  KC_P ,   KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                                     KC_H,    KC_J ,   KC_K ,   KC_L ,  KC_SCLN, CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V,   KC_B,          KC_LBRC, OSL(FKEYS),           KC_CAPS, KC_RBRC, KC_N,    KC_M ,   KC_COMM, KC_DOT ,KC_SLSH, RSA_T(KC_ESC),
-                               LCTL_T(KC_ENTER), KC_LALT , KC_LGUI, KC_ENTER, MO(SYM),         MO(NAV)  , TG(MOUSE) , KC_SPC ,  KC_RGUI, KC_GRV
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,    KC_T ,                                                     KC_Y,    KC_U ,   KC_I ,   KC_O ,  KC_P ,   KC_BSPC,
+     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,    KC_G ,                                                     KC_H,    KC_J ,   KC_K ,   KC_L ,  KC_SCLN, CTL_QUOT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,    KC_B , KC_LBRC, OSL(FKEYS),              KC_CAPS, KC_RBRC, KC_N,    KC_M ,   KC_COMM, KC_DOT ,KC_SLSH, RSA_T(KC_ESC),
+                       LCTL_T(KC_ENTER), KC_LALT , KC_LGUI, KC_ENTER, MO(SYM),         MO(NAV)  , LT(MOUSE, KC_SPC) , KC_RGUI ,   KC_LALT , KC_GRV
     ),
 
 /*
  * Numbers and symbols
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |    `   |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |   =    |
+ * |    `   |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |    ~   |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |   +    |
+ * |    ~   |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |    |   |   \  |      |      |  -   |  [   |  {   |      |  |      |   }  |   ]  |  _   |      |      |      |        |
+ * |    |   |   \  |  =   |  -   |  (   |  [   |  {   |      |  |      |   }  |   ]  |  )   |  _   |   +  |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  | Fn   |      |      |      |      |
- *                        |      |      |      |      |      |  | layer|      |      |      |      |
+ *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SYM] = LAYOUT(
-      KC_GRV ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_EQL ,
-     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-     KC_PIPE , KC_BSLS, _______, _______, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, _______, _______, _______, _______,
+      KC_GRV ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , _______ ,
+     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     KC_PIPE , KC_BSLS, KC_EQL , KC_LPRN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_RPRN, KC_PLUS, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 /*
