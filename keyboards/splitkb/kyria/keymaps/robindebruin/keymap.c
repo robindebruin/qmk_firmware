112df2eaaec9d91cdf0266f5c914be973cc73d9d
Robin de Bruin <robin_de_bruin@live.nl>
2022-11-30 12:02:54 +0100

    shift left

--- keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
+++ keyboards/splitkb/kyria/keymaps/robindebruin/keymap.c
@@ -51,6 +51,8 @@ enum custom_keycodes { Undo, Cut, Copy, Paste, NxtWord, PrvWord };
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
 
+// LT(MOUSE, KC_SPC)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -68,10 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,    KC_T ,                                                     KC_Y,    KC_U ,   KC_I ,   KC_O ,  KC_P ,   KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,    KC_G ,                                                     KC_H,    KC_J ,   KC_K ,   KC_L ,  KC_SCLN, CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,    KC_B , KC_LBRC, OSL(FKEYS),              KC_CAPS, KC_RBRC, KC_N,    KC_M ,   KC_COMM, KC_DOT ,KC_SLSH, RSA_T(KC_ESC),
-                       LCTL_T(KC_ENTER), KC_LALT , KC_LGUI, KC_ENTER, MO(SYM),         MO(NAV)  , LT(MOUSE, KC_SPC) , KC_RGUI ,   KC_LALT , KC_GRV
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,           KC_R ,            KC_T ,                                                      KC_Y,    KC_U ,   KC_I ,            KC_O ,     KC_P ,       KC_BSPC,
+     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,  LT(MOUSE,KC_F),    LT(NAV,KC_G) ,                                                      KC_H,    KC_J ,   KC_K ,            KC_L ,   KC_SCLN,      CTL_QUOT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,           KC_V ,            KC_B , KC_LBRC, OSL(FKEYS),               KC_CAPS, KC_RBRC, KC_N,    KC_M ,   KC_COMM,        KC_DOT ,   KC_SLSH, RSA_T(KC_ESC),
+                       LCTL_T(KC_ENTER),        KC_LALT ,          KC_LGUI, KC_ENTER,   MO(SYM),            MO(NAV)  , LT(MOUSE, KC_SPC) , LT(KC_RGUI,KC_SPC) ,   KC_LALT , KC_GRV  
     ),
 
 /*
@@ -100,22 +102,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |        |      |      |      |      |      |                              | PgUp | Home |   ↑  | End  | VolUp| Delete |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  GUI |  Alt | Ctrl | Shift|      |                              | PgDn |  ←   |   ↓  |   →  | VolDn| Insert |
+ * |   shift|  ctrl| Alt  | cmnd |      |      |                              | PgDn |  ←   |   ↓  |   →  | VolDn| Insert |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |ScLck |  |      |      | Pause|M Prev|M Play|M Next|VolMut| PrtSc  |
+ * |        |      |      |      |      |      |      |      |  |      |      | Pause|M Prev|M Play|M Next|VolMut| PrtSc  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, _______,
-      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, _______,
+      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, _______,
+      KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 /*
-     * Raise Layer: Number keys, media, navigation
+     * MOUSE
      *
      * ,-------------------------------------------.                              ,-------------------------------------------.
      * |        |   1  |  2   |  3   |  4   |  5   |                              |  6   |  7   |  8   |  9   |  0   |        |
